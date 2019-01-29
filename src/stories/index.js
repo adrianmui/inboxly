f5ab924a78454a270c7ed7b4e0ff498c6cc8766a
adrian mui <adrian.mui@gmail.com>
2019-01-28 21:20:39 -0800

    organized storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,30 +3,38 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withStyles } from '@material-ui/core';
 
 import { Button, Welcome } from '@storybook/react/demo';
 import { Switch } from '../components';
 import LeftNav from '../LeftNav';
+import Navbar from '../Navbar';
 
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
+const styles = {};
+
+const Playground = withStyles(styles)(({ children, classes }) => (
+  <div classes={classes}>{children}</div>
 ));
 
-storiesOf('Button', module)
-  .add('with text', () => (
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Playground>
+    <Welcome showApp={linkTo('Button')} />
     <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
     </Button>
-  ));
-
-storiesOf('Switch', module).add('base', () => <Switch />);
-storiesOf('Sandbox', module).add('playground', () => (
-  <div>
-    <LeftNav />
-  </div>
+  </Playground>
+));
+storiesOf('Input', module).add('Switch', () => <Switch />);
+storiesOf('Nav', module)
+  .add('App Nav', () => (
+    <Playground>
+      <Navbar />
+    </Playground>
+  ))
+  .add('Left Nav', () => <LeftNav />);
+storiesOf('_Playground', module).add('play', () => (
+  <Playground>sdlfksdjf</Playground>
 ));
